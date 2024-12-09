56b4370693d3c4abd1b07332cc8c2bad911b9530
Sidi Maadh <155539380+sidichrifahmedmaadh@users.noreply.github.com>
2024-12-09 22:56:55 +0000

    Delete GestionEtudiants.sql

--- GestionEtudiants.sql
+++
@@ -1,9 +0,0 @@
-create database GestionEtudiants;
-use GestionEtudiants;
-
-create table GestionEtudiants (id varchar(15) primary key,
-							   nom varchar(15),
-							   prenom varchar(15),
-                               filiere varchar(15));
-
-select * from GestionEtudiants;
