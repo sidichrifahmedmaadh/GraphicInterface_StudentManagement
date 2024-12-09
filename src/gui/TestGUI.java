5a65d14ef350d5bf4ce8e303028edb49ea09bed6
Sidi Maadh <155539380+sidichrifahmedmaadh@users.noreply.github.com>
2024-12-09 23:21:14 +0000

    Add files via upload

---
+++ src/gui/TestGUI.java
@@ -0,0 +1,11 @@
+import db.*;
+import metier.*;
+import dao.*;
+import gui.*;
+
+public static void main(String[] args) {
+        new etudiantFenetre();
+        new professeurFenetre();
+        new departementFenetre();
+
+    }
